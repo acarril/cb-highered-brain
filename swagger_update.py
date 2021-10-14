614964efeb036c4834467e3cf638ded562b24973
acarril <acarril@princeton.edu>
2021-10-14 18:14:59 -0400

    add option to remove OPTIONS element from json config

--- swagger_update.py
+++ swagger_update.py
@@ -1,6 +1,8 @@
 import subprocess
 import argparse
 import boto3
+import json
+from collections.abc import MutableMapping
 
 REST_API_ID = 'j0z5xz882m'
 
@@ -11,20 +13,34 @@ def export_api(id=REST_API_ID):
         stageName='api',
         exportType='swagger'
     )
-    json_content = response['body'].read().decode('utf-8')
-    return json_content
+    body = response['body']
+    return body
 
 def sync_swagger_s3():
     cmd = 'aws s3 sync docs/swagger_dist s3://cb-highered-brain-swagger'
     subprocess.run(cmd.split())
 
+def delete_keys_from_dict(dictionary, keys):
+    keys_set = set(keys)  # just an optimization for the "if key in keys" lookup
+    modified_dict = {}
+    for key, value in dictionary.items():
+        if key not in keys_set:
+            if isinstance(value, MutableMapping):
+                modified_dict[key] = delete_keys_from_dict(value, keys_set)
+            else:
+                modified_dict[key] = value
+    return modified_dict
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--rest-api-id', default=REST_API_ID)
+    parser.add_argument('--no-options', action='store_true')
     args = parser.parse_args()
-    json_content = export_api(id=args.rest_api_id)
+    json_content = json.load(export_api(id=args.rest_api_id))
+    if args.no_options:
+        json_content = delete_keys_from_dict(json_content, ['options'])
     with open('docs/swagger_dist/docs/api-cb-highered-brain.json', 'w') as file:
-        file.write(json_content)
+        json.dump(json_content, file, indent=2)
     sync_swagger_s3()
 
 if __name__ == '__main__':
